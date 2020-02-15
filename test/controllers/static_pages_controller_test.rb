915021b6550a92239fb8f84b38d68de38b2278fb
moco1002 <moco1002web@gmail.com>
2020-02-16 00:47:40 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get root" do
+   get static_pages_home_url
+   assert_response :success
+ end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -22,5 +28,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   assert_response :success
   assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+  get static_pages_contact_url
+  assert_response :success
+  assert_select "title", "Contact | #{@base_title}"
+  end
+
 
 end
