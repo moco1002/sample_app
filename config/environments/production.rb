891786d79880687b5ae7af4bd80ede419afe60f8
moco1002 <moco1002web@gmail.com>
2020-02-26 19:31:02 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://aqueous-wave-27486.herokuapp.com'
+  host = 'aqueous-wave-27486.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
