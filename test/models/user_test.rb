37373bdd183bbf3e701fe8ed3f0020a9664fa793
moco1002 <moco1002web@gmail.com>
2020-03-09 19:33:57 +0900

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,4 +63,12 @@ test "email addresses should be saved as lower-case" do
    assert_not @user.authenticated?(:remember, '')
 end
 
+test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
